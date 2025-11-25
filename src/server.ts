6cb431f1eb2d725945c152c445be1c1287ef01d9
Arthur Macedo <arthurmaceddo@gmail.com>
2025-11-25 19:38:17 -0300

    cors-added

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import "dotenv/config"
 import express from 'express'
+import cors from 'cors' // <--- 1. IMPORTAR O CORS
 import userRoutes from './routes/user.routes.js'
 import clientRoutes from './routes/client.routes.js'
 import serviceRoutes from './routes/service.routes.js'
 import budgetRoutes from './routes/budget.routes.js'
 
-
-
 const app = express()
 const PORT = 3000
 
 app.use(express.json())
+app.use(cors()) // <--- 2. USAR O CORS (Permite que o React acesse o Backend)
+
 app.use(userRoutes)
 app.use(clientRoutes)
 app.use(serviceRoutes)
